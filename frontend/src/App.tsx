33b31d084085f4cb2c7c687b4722a383d6ea4ffb
bernardorz <bejusouh123@gmail.com>
2021-05-08 14:08:20 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from 'components/Navbar/index';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 
 function App() {
@@ -9,7 +11,26 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Ola mundo</h1>
+        <h1 className="text-primary">Dashboard</h1>
+
+        <div className="row px-3">
+           <div className="col-sm-6">
+
+             <h5>Todas vendas</h5>
+              <BarChart />
+           </div>
+           <div className="col-sm-6">
+
+             <h5>Todas vendas</h5>
+             <DonutChart />
+           </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">
+            Todas as vendas
+          </h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
  return (
     <footer className="footer mt-auto py-3 bg-dark">
     <div className="container">
-      <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
-      <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      <p className="text-light">App desenvolvido por <a href="https://github.com/bernardorz" target="_blank" rel="noreferrer">Bernardo Rizzatti</a></p>
+      <p className="text-light"><small><strong></strong><br/>
+        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/bernardozjj" target="_blank" rel="noreferrer"> @bernardorzjj </a></small></p>
     </div>
   </footer>
  )
