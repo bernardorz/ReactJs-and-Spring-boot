9afef15824d4fd384cdef8d68b0cec90e036e9f1
bernardorz <bejusouh123@gmail.com>
2021-05-09 15:29:27 -0300

    redes sociais

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
  return (
     <footer className="footer mt-auto py-3 bg-dark">
     <div className="container">
-      <p className="text-light">App desenvolvido por <a href="https://github.com/bernardorz" target="_blank" rel="noreferrer">Bernardo Rizzatti</a></p>
+      <p className="text-light"> Linkedin <a href="https://www.linkedin.com/in/bernardo-rizzatti-6382ab1a6/" target="_blank" rel="noreferrer">Bernardo Rizzatti</a></p>
       <p className="text-light"><small><strong></strong><br/>
-        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/bernardozjj" target="_blank" rel="noreferrer"> @bernardorzjj </a></small></p>
+        Github  <a href="https://github.com/bernardorz" target="_blank" rel="noreferrer"> @bernardorzjj </a></small></p>
     </div>
   </footer>
  )
